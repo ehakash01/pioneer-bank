a80252b1299651fee4b92ceca8ac8ba9d2fb4f98
Eh Akash <ehakash01@gmail.com>
2021-01-27 20:28:32 +0600

    pioneer bank full functionality updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// login button event handler
+
 const loginButton = document.getElementById("login");
 loginButton.addEventListener("click", function() {
     const loginArea = document.getElementById("login-area");
@@ -5,3 +7,37 @@ loginButton.addEventListener("click", function() {
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
 })
+
+// deposit button event handler
+
+const depositButton = document.getElementById("addDeposit");
+depositButton.addEventListener("click", function() {
+    const depositNumber = getInputNumber("depositAmount");
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    document.getElementById("depositAmount").value = "";
+})
+
+// withdraw button event handler
+
+const withdrawButton = document.getElementById("addWithdraw");
+withdrawButton.addEventListener("click", function() {
+    const withdrawNumber = getInputNumber("withdrawAmount")
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1 * withdrawNumber);
+    document.getElementById("withdrawAmount").value = "";
+})
+
+function getInputNumber(id) {
+    const Amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(Amount);
+    return amountNumber;
+}
+
+
+function updateSpanText(id, addedNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = addedNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
